157008e3df788f785e3738361207bc408fbf0a72
Dmitry Frank <mail@dmitryfrank.com>
2018-03-16 17:35:32 +0200

    Add glide config and CI script

---
+++ .ci/build.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# This is run on Jenkins
+set -e
+
+if [ -z "${WORKSPACE}" ]
+then
+  echo "Jenkins WORKSPACE env var not set - exiting" >&2
+  exit 1
+fi
+
+# Set up GOPATH
+export GOPATH=$WORKSPACE/.go
+mkdir -p $GOPATH/src/github.com/cryptowatch
+mkdir -p $GOPATH/bin
+export PATH=$PATH:$GOPATH/bin
+
+ln -s -f $WORKSPACE $GOPATH/src/github.com/cryptowatch/stream-client-go
+cd $GOPATH/src/github.com/cryptowatch/stream-client-go
+
+# Set up go get creds
+git config --global url."git@github.com:".insteadOf "https://github.com/"
+
+# Install glide
+curl https://glide.sh/get | sh
+
+glide install
+
+# Run tests
+make test
